80877646cfd438f565d179efd7d5055004854f53
dev3 <dev3@DESKTOP-BSHTT23>
2022-10-06 18:45:48 +0200

    small numbers passed.

--- kata4/src/Next bigger number with the same digits/kata.ts
+++ kata4/src/Next bigger number with the same digits/kata.ts
@@ -1,7 +1,7 @@
 export function nextBigger(n: number): number {
   let digits: number[] = n.toString().split('').map(x => parseInt(x));
   if (!(digits.every(v => v === digits[0])) && !(digits.length === 1)) {
-    let index: number = digits.length
+     let index: number = digits.length - 1
     while (index > 0) {
       if (digits[index] === digits[index - 1]) {
         index -= 1;
@@ -11,9 +11,9 @@ export function nextBigger(n: number): number {
         let num: number = digits[index];
         digits[index] = digits[index - 1];
         digits[index - 1] = num;
+        return parseInt(digits.join(''));
       }
     }
-    return parseInt(digits.join(''));
   }
   return -1;
 }
