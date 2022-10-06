e41b917ca561e54d1cec4c60c12a629ecd5eb004
dev3 <dev3@DESKTOP-BSHTT23>
2022-10-06 18:29:09 +0200

    Next bigger number with the same digits

--- kata4/src/Next bigger number with the same digits/kata.ts
+++ kata4/src/Next bigger number with the same digits/kata.ts
@@ -1,3 +1,19 @@
 export function nextBigger(n: number): number {
-  return parseInt(n.toString().split('').sort((n1,n2) => parseInt(n1) > parseInt(n2) ? -1 : 1).join(''));
+  let digits: number[] = n.toString().split('').map(x => parseInt(x));
+  if (!(digits.every(v => v === digits[0])) && !(digits.length === 1)) {
+    let index: number = digits.length
+    while (index > 0) {
+      if (digits[index] === digits[index - 1]) {
+        index -= 1;
+      } else if (digits[index] < digits[index - 1]) {
+        index -= 1;
+      } else {
+        let num: number = digits[index];
+        digits[index] = digits[index - 1];
+        digits[index - 1] = num;
+      }
+    }
+    return parseInt(digits.join(''));
+  }
+  return -1;
 }
